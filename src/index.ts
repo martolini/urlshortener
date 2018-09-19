9562d343813ee58e71461f6e1fa918901a710b2e
Linn Kristin <linnkri91@gmail.com>
2018-09-19 22:08:18 +0200

    Added logging on startup (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import server from './server';
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
